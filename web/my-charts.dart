6827d0d9b88866221c2167d1f615984c76b3f1ea
Gold-miner354 <101792617+Gold-miner354@users.noreply.github.com>
2022-07-01 20:47:01 +0800

    new

--- web/my-charts.dart
+++ web/my-charts.dart
@@ -10,11 +10,11 @@ void main(List<String> args) {
   dartStatus.text = 'Dart is running!';
 
   var myData = JsObject.jsify([
-    {'year': '2001', 'population': 2},
-    {'year': '2002', 'population': 4},
-    {'year': '2003', 'population': 6},
-    {'year': '2004', 'population': 8},
-    {'year': '2005', 'population': 10},
+    {'year': '2001', 'population': 1},
+    {'year': '2002', 'population': 2},
+    {'year': '2003', 'population': 3},
+    {'year': '2004', 'population': 4},
+    {'year': '2005', 'population': 5},
   ]);
 
   var chart = context['chart'];
